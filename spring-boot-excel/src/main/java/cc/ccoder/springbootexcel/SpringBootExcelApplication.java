4a708ae854763d8d7ec797ee97c53c672ca0788f
chencong-plan <1042738887@qq.com>
2019-01-07 16:34:30 +0800

    spring-boot-excel 提交项目配置文件

---
+++ spring-boot-excel/src/main/java/cc/ccoder/springbootexcel/SpringBootExcelApplication.java
@@ -0,0 +1,18 @@
+package cc.ccoder.springbootexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ccoder.cc
+ * @since 2019-1-7 16:22:07
+ */
+@SpringBootApplication
+public class SpringBootExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExcelApplication.class, args);
+    }
+
+}
+
