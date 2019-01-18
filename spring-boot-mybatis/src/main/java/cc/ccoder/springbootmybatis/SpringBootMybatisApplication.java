f597dac56ad039d9df9dab1e72d3e8bb550e90ed
chencong-plan <1042738887@qq.com>
2019-01-18 15:26:54 +0800

    添加spring-boot-mybatis 注解无配置版

---
+++ spring-boot-mybatis/src/main/java/cc/ccoder/springbootmybatis/SpringBootMybatisApplication.java
@@ -0,0 +1,14 @@
+package cc.ccoder.springbootmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisApplication.class, args);
+    }
+
+}
+
